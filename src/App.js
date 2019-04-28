bf03408985e98678fc0d12236cc1ead7b250ee56
Karolin Rafalski <kr2174@caa.columbia.edu>
2019-04-28 13:27:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppRouter from './components/AppRouter.js'
 
 function App() {
   return (
     <div className="App">
+    <AppRouter />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
